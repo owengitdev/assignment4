c911a83fecdc7dbed3898db7741ee25e5bff6389
owengitdev <yanyida19790228@gmail.com>
2025-09-27 23:42:42 -0400

    Update timed_challenge.py

--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,10 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+#Group 4: Last-In, First-Out Logic
+# 13. Balanced Symbols
+# Check if the brackets in a string are balanced.
+# Input: "{[()]}"
+# Output: True
+# Input: "{[(])}"
+# Output: False
